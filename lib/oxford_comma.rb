9618b482c63604932d91f08fdb550847ce6745de
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2020-05-28 23:09:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length==2
   array[0] + " and " + array[1]
   else
-    array[-1]="and" + array[-1]
-    array.join(",")
+    array[-1]="and " + array[-1]
+    array.join(", ")
   end
 end
